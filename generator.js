762fc1e98fc76d70de1d55e20966bb0ea2f03fa3
Cody Klimkofski <50000309+CodyKlimkofski@users.noreply.github.com>
2020-11-30 13:58:23 -0500

    Updated generator.js
    generator.js now returns null string (skips for loop) when no password character categories are selected, made general improvements to code

--- generator.js
+++ generator.js
@@ -1,21 +1,24 @@
 function generatePassword() {
     let charList = [];
     let resultString = "";
-    if (document.getElementById("lowerCheck").checked == true) {
+    if (document.getElementById("lowerCheck").checked) {
         charList.push("abcdefghijklmnopqrstuvwxyz");
-    } if (document.getElementById("upperCheck").checked == true) {
+    } if (document.getElementById("upperCheck").checked) {
         charList.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    } if (document.getElementById("numsCheck").checked == true) {
+    } if (document.getElementById("numsCheck").checked) {
         charList.push("1234567890");
-    } if (document.getElementById("symbCheck").checked == true) {
+    } if (document.getElementById("symbCheck").checked) {
         charList.push("!@#$%^&*(),.<>/?`-_");
     }
 
-    for (var i = 0; i < parseInt(document.getElementById("rangeSlider").value); i++) {
-        let randomNum = Math.floor(Math.random() * charList.length);
-        let randomChar = Math.floor(Math.random() * charList[randomNum].length);
-        resultString += charList[randomNum][randomChar];
-    }
+    if (charList.length > 0) {
+        for (var i = 0; i < parseInt(document.getElementById("rangeSlider").value); i++) {
+            let randomNum = Math.floor(Math.random() * charList.length);
+            let randomChar = Math.floor(Math.random() * charList[randomNum].length);
+            resultString += charList[randomNum][randomChar];
+        }
+    }    
+
     document.getElementById("passwordBox").value = resultString;
 }
 
